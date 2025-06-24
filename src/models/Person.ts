2917355716e3e5de29df0c04deb1c913f31a9597
Cloyd Abad <abadcloyd@gmail.com>
2025-06-24 23:45:10 +0800

    test(Person): add test to the length of the name field of the Person object

--- src/models/Person.ts
+++ src/models/Person.ts
@@ -1,6 +1,15 @@
 export class Person {
   name: string;
-  constructor(name: string) {
+  // wallet_balance: number;
+
+  constructor(name: string, wallet_balance?: number) {
+    if (name.length < 2) {
+      throw new Error("Name should be longer than 2 characters");
+    }
+    if (name.length > 20) {
+      throw new Error("Name should not be longer than 20 characters");
+    }
     this.name = name;
+    // this.wallet_balance = wallet_balance || 0;
   }
 }
--- src/models/__tests__/Person.test.ts
+++ src/models/__tests__/Person.test.ts
@@ -5,3 +5,15 @@ test("Person should have a name", () => {
 
   expect(createdPerson.name).toBe("Cloyd");
 });
+
+test("Person should throw an error when the length of name is less than 2", () => {
+  expect(() => {
+    const createdPerson = new Person("t");
+  }).toThrow("Name should be longer than 2 characters");
+});
+
+test("Person should throw an error when the length of name is greater than 20", () => {
+  expect(() => {
+    const createdPerson = new Person("123456789012345678901");
+  }).toThrow("Name should not be longer than 20 characters");
+});
