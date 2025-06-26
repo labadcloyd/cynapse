8791484c4337538a1b7ca3bb2c3586bec96c5ade
Cloyd Abad <abadcloyd@gmail.com>
2025-06-26 11:15:52 +0800

    test(angpao): add test for adding friends in angpao

--- src/models/Angpao.ts
+++ src/models/Angpao.ts
@@ -4,12 +4,15 @@ export class Angpao {
   ask: (question: string) => Promise<string>;
   calcAmount: (balance: number) => number;
   owner!: Person;
+  friends: Person[];
+
   constructor(
     ask: (question: string) => Promise<string>,
     calcAmount: (balance: number) => number
   ) {
     this.ask = ask;
     this.calcAmount = calcAmount;
+    this.friends = [];
   }
 
   async init() {
@@ -18,5 +21,22 @@ export class Angpao {
       "What is your desired wallet balance? "
     );
     this.owner = new Person(name, Number(wallet_balance));
+  }
+
+  async initializeFriends() {
+    console.log("Let's add your friends!");
+    const name = await this.ask("What is your friend's name? ");
+
+    this.friends.push(new Person(name));
+
+    let repeat = await this.ask("Do you want to add more friends? (Y/N)");
+
+    while (repeat.toLowerCase() === "y") {
+      const name = await this.ask("What is your friend's name? ");
+
+      this.friends.push(new Person(name));
+
+      repeat = await this.ask("Do you want to add more friends? (Y/N)");
+    }
   }
 }
--- src/models/__tests__/Angpao.test.ts
+++ src/models/__tests__/Angpao.test.ts
@@ -14,3 +14,28 @@ test("Angpao should ask for the owner's name and balance", async () => {
   expect(owner.name).toBe("Cloyd");
   expect(owner.wallet_balance).toBe(20);
 });
+
+test("Angpao should ask for the list of friends", async () => {
+  const mockAsk = jest
+    .fn()
+    .mockResolvedValueOnce("Cloyd")
+    .mockResolvedValueOnce("20")
+    .mockResolvedValueOnce("Brew")
+    .mockResolvedValueOnce("Y")
+    .mockResolvedValueOnce("Lance")
+    .mockResolvedValueOnce("y")
+    .mockResolvedValueOnce("Martus")
+    .mockResolvedValueOnce("N");
+
+  const AngpaoAlgo = new Angpao(mockAsk, CalcDistribution);
+  await AngpaoAlgo.init();
+  await AngpaoAlgo.initializeFriends();
+
+  const owner = AngpaoAlgo.owner;
+  expect(owner.name).toBe("Cloyd");
+  expect(owner.wallet_balance).toBe(20);
+  expect(AngpaoAlgo.friends.length).toBe(3);
+  expect(AngpaoAlgo.friends[0].name).toBe("Brew");
+  expect(AngpaoAlgo.friends[1].name).toBe("Lance");
+  expect(AngpaoAlgo.friends[2].name).toBe("Martus");
+});
